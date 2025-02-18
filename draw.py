2ad18b13d16c2eea7722d8a5b140622c9d8fd925
Zaenal Maestro <zaenal.official.1@gmail.com>
2025-02-18 17:05:29 +0700

    draw line from frame video

--- draw.py
+++ draw.py
@@ -3,20 +3,40 @@ import numpy as np
 
 # Membuat citra kosong
 img = np.zeros((512, 512, 3), np.uint8)
-def RGB(event, x, y, flags, param):
+def checkMousePosition(event, x, y, flags, param):
     if event == cv2.EVENT_MOUSEMOVE :  
-        colorsBGR = [x, y]
-        print(colorsBGR)
+        print(f'x:{x} | y:{y}')
 
-# Titik-titik yang membentuk poligon
 # [x, y]
-pts = np.array([[107, 166], [357, 166], [434, 421], [86, 421]], np.int32)
+illegal_area = np.array([[60, 94], [272, 153], [273, 275], [46, 222]], np.int32)
 
-# Menggambar poligon
-cv2.polylines(img, [pts], True, (0, 255, 0), 1)
-cv2.namedWindow('RGB')
-cv2.setMouseCallback('RGB', RGB)
-# Menampilkan citra
-cv2.imshow('RGB', img)
-cv2.waitKey(0)
+# cv2.polylines(img, [illegal_area], True, (0, 255, 0), 1)
+# cv2.namedWindow('Parking Area')
+# cv2.setMouseCallback('Parking Area', checkMousePosition)
+# # Menampilkan citra
+# cv2.imshow('Parking Area', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+cap = cv2.VideoCapture('illegal-parking.mp4')
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    cv2.namedWindow('Parking Area')
+    cv2.setMouseCallback('Parking Area', checkMousePosition)
+
+    cv2.polylines(frame, [illegal_area], True, (0, 0, 255), 3)
+    cv2.namedWindow('Parking Area')
+    cv2.imshow('Parking Area', frame)
+    # cv2.waitKey(0)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+
+
+cap.release()
 cv2.destroyAllWindows()
+
